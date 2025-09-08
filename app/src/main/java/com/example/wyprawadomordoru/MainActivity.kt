050fecc917634e3f823d342b5762d37f6f075c5b
Mateusz <mateusz.budek@cyfroweszkoly.pl>
2025-09-08 17:51:29 +0200

    Connect views

--- app/src/main/java/com/example/wyprawadomordoru/MainActivity.kt
+++ app/src/main/java/com/example/wyprawadomordoru/MainActivity.kt
@@ -1,8 +1,18 @@
 package com.example.wyprawadomordoru
 
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.Chronometer
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.RadioGroup
+import android.widget.RatingBar
+import android.widget.SeekBar
+import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Button
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,5 +26,36 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val characterNameEditText = findViewById<EditText>(R.id.characterName_EditText)
+        val characterRaceSpinner = findViewById<Spinner>(R.id.characterRace_Spinner)
+        val calendarButton = findViewById<Button>(R.id.calendarOpen_Button)
+        val elvesSecretPathSwitch = findViewById<Switch>(R.id.turnOnElvesSecretPaths_Switch)
+        val itemElvesCape = findViewById<CheckBox>(R.id.item_elvesCape_CheckBox)
+        val itemLembases = findViewById<CheckBox>(R.id.item_lembases_CheckBox)
+        val itemTorch = findViewById<CheckBox>(R.id.item_torch_CheckBox)
+        val marchRadioGroup = findViewById<RadioGroup>(R.id.march_RadioGroup)
+        val marchTimeSeekBar = findViewById<SeekBar>(R.id.march_time_SeekBar)
+        val moralesRatingBar = findViewById<RatingBar>(R.id.morales_RatingBar)
+
+
+        val characterNameTextBox = findViewById<TextView>(R.id.summary_characterName_TextBox)
+        val characterRaceTextBox = findViewById<TextView>(R.id.summary_characterRace_TextBox)
+        val datetimeTextBox = findViewById<TextView>(R.id.summary_datetime_TextBox)
+        val elvesPathsTextBox = findViewById<TextView>(R.id.summary_elvesPaths_TextBox)
+        val equipmentTextBox = findViewById<TextView>(R.id.summary_equipment_TextBox)
+        val marchPriorityTextBox = findViewById<TextView>(R.id.summary_marchPriority_TextBox)
+        val marchTimeTextBox = findViewById<TextView>(R.id.summary_marchTime_TextBox)
+        val teamMoralesTextBox = findViewById<TextView>(R.id.summary_teamMorales_TextBox)
+
+
+        val trainingChronometer = findViewById<Chronometer>(R.id.Training_Chronometer)
+        val chronometerStartButton = findViewById<Button>(R.id.Chronometer_Start_Button)
+        val chronometerStopButton = findViewById<Button>(R.id.Chronometer_Stop_Button)
+        val chronometerResetButton = findViewById<Button>(R.id.Chronometer_Reset_Button)
+
+
+        val progressBar = findViewById<ProgressBar>(R.id.countdown_ProgressBar)
+        val progressBarStartButton = findViewById<Button>(R.id.startCountdown_Button)
     }
 }
