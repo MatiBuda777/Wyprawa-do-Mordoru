17470238d569b7c984aa06337c7bd8de447dc987
mateuszbudek <mateusz.budek@cyfroweszkoly.pl>
2025-09-10 12:30:33 +0200

    Continue building app functionality

--- app/src/main/java/com/example/wyprawadomordoru/MainActivity.kt
+++ app/src/main/java/com/example/wyprawadomordoru/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.wyprawadomordoru
 
 import android.icu.util.Calendar
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.CheckBox
 import android.widget.Chronometer
 import android.widget.EditText
@@ -9,7 +14,9 @@ import android.widget.ProgressBar
 import android.widget.RadioGroup
 import android.widget.RatingBar
 import android.widget.SeekBar
+import android.widget.Spinner
 import android.widget.Switch
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Button
@@ -17,6 +24,24 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var characterRaces = arrayOf(
+        "Człowiek",
+        "Elf",
+        "Krasnolud",
+        "Hobbit",
+        "Ork",
+        "Goblin",
+        "Ent",
+        "Troll",
+        "Nazgûl",
+        "Maiar",
+        "Balrog",
+        "Uruk-hai",
+        "Smok"
+    )
+    private var ChronometerRunning = false
+    private var ChronometerPauseOffset: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -27,10 +52,51 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+
         val characterNameEditText = findViewById<EditText>(R.id.characterName_EditText)
+        val characterNameTextBox = findViewById<TextView>(R.id.summary_characterName_TextBox)
+        characterNameEditText.setOnClickListener {
+            characterNameTextBox.text = "Imię: ${characterNameEditText.text}"
+        }
+
+
         val characterRaceSpinner = findViewById<Spinner>(R.id.characterRace_Spinner)
+        val characterRaceTextBox = findViewById<TextView>(R.id.summary_characterRace_TextBox)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, characterRaces)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        characterRaceSpinner.adapter = adapter
+        characterRaceSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                characterRaceTextBox.text = "Rasa: ${characterRaces[position]}"
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+
         val calendarButton = findViewById<Button>(R.id.calendarOpen_Button)
-        val elvesSecretPathSwitch = findViewById<Switch>(R.id.turnOnElvesSecretPaths_Switch)
+        val datetimeTextBox = findViewById<TextView>(R.id.summary_datetime_TextBox)
+
+        val elvesPathsSwitch = findViewById<Switch>(R.id.turnOnElvesSecretPaths_Switch)
+        val elvesPathsTextBox = findViewById<TextView>(R.id.summary_elvesPaths_TextBox)
+        elvesPathsSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                elvesPathsSwitch.text = "Tak"
+                elvesPathsTextBox.text = "Ścieżki elfów włączone"
+            }
+            if (!isChecked) {
+                elvesPathsSwitch.text = "Nie"
+                elvesPathsTextBox.text = "Ścieżki elfów wyłączone"
+            }
+        }
+
         val itemElvesCape = findViewById<CheckBox>(R.id.item_elvesCape_CheckBox)
         val itemLembases = findViewById<CheckBox>(R.id.item_lembases_CheckBox)
         val itemTorch = findViewById<CheckBox>(R.id.item_torch_CheckBox)
@@ -39,20 +105,40 @@ class MainActivity : AppCompatActivity() {
         val moralesRatingBar = findViewById<RatingBar>(R.id.morales_RatingBar)
 
 
-        val characterNameTextBox = findViewById<TextView>(R.id.summary_characterName_TextBox)
-        val characterRaceTextBox = findViewById<TextView>(R.id.summary_characterRace_TextBox)
-        val datetimeTextBox = findViewById<TextView>(R.id.summary_datetime_TextBox)
-        val elvesPathsTextBox = findViewById<TextView>(R.id.summary_elvesPaths_TextBox)
+
         val equipmentTextBox = findViewById<TextView>(R.id.summary_equipment_TextBox)
         val marchPriorityTextBox = findViewById<TextView>(R.id.summary_marchPriority_TextBox)
         val marchTimeTextBox = findViewById<TextView>(R.id.summary_marchTime_TextBox)
         val teamMoralesTextBox = findViewById<TextView>(R.id.summary_teamMorales_TextBox)
 
 
-        val trainingChronometer = findViewById<Chronometer>(R.id.Training_Chronometer)
+
+
+        val chronometer = findViewById<Chronometer>(R.id.Training_Chronometer)
         val chronometerStartButton = findViewById<Button>(R.id.Chronometer_Start_Button)
         val chronometerStopButton = findViewById<Button>(R.id.Chronometer_Stop_Button)
         val chronometerResetButton = findViewById<Button>(R.id.Chronometer_Reset_Button)
+
+        chronometerStartButton.setOnClickListener {
+            if (!ChronometerRunning) {
+                chronometer.base = SystemClock.elapsedRealtime() - ChronometerPauseOffset
+                chronometer.start()
+                ChronometerRunning = true
+            }
+        }
+        chronometerStopButton.setOnClickListener {
+            if (ChronometerRunning) {
+                ChronometerPauseOffset = SystemClock.elapsedRealtime() - chronometer.base
+                chronometer.stop()
+                ChronometerRunning = false
+            }
+        }
+        chronometerResetButton.setOnClickListener {
+            chronometer.base = SystemClock.elapsedRealtime()
+            chronometer.stop()
+            ChronometerPauseOffset = 0
+            ChronometerRunning = false
+        }
 
 
         val progressBar = findViewById<ProgressBar>(R.id.countdown_ProgressBar)
